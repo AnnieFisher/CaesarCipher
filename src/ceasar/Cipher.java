4fe7503dc514dc7030fe1da562a7c8be97b247ff
AnnieFisher <annielynn.fisher@gmail.com>
2016-07-25 21:17:43 -0600

    Encryption works well. Decryption just a reprint of original message

--- src/ceasar/Cipher.java
+++ src/ceasar/Cipher.java
@@ -4,12 +4,42 @@ import java.util.Scanner;
 
 public class Cipher {
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("enter what to encrypt ");   
+		String originalMessage = kb.nextLine();
 		
-	Scanner kb = new Scanner(System.in);
-	System.out.println("Enter message");
-	String message = kb.next();
-	char c = message.charAt(0);
-	
-	
+		kb.close();
+		
+		// prints out the encrypted message
+		String encrypt = encrypt(originalMessage, 3);
+		System.out.println("Encrypted message:"+encrypt);
+		
+		//prints out the decrypted message
+		String decrypt = decrypt(originalMessage, 3);
+		System.out.println("Decrypted Message "+decrypt);
 	}
-}
+	// method to encrypt
+	//create an array from the user input
+	static String encrypt(String originalMessage, int shift) {
+		char[] originalMessageCharArray = originalMessage.toCharArray();
+		int firstCharIndex = 'A';   //create an index for the chars
+		int offset = ('z'-'A'); //need to offset for the mod in next steps
+		
+		for (int i = 0; i < originalMessageCharArray.length; i++) {
+			
+			char oldCharIndex = originalMessageCharArray[i]; //index now equals the letters
+			int oldIndexInAlphabet = oldCharIndex - firstCharIndex; //shows how many letters are involved
+			int newIndexInAlphabet = (oldIndexInAlphabet + shift) % offset;
+			char newCharIndex = (char)(firstCharIndex + newIndexInAlphabet);
+			originalMessageCharArray[i] = newCharIndex;
+		}
+		return new String(originalMessageCharArray);
+	}
+	static String decrypt(String encrypt, int shift) {
+		
+		return encrypt;
+	}
+
+
+
+}	
