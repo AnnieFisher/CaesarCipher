2046f58fd7e7e1beb912cdcec170d05f67a36a4e
Steve Nagle <steve.nagle@outlook.com>
2016-07-25 18:16:39 -0600

    New update for homework

--- src/ceasar/Cipher.java
+++ src/ceasar/Cipher.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Cipher {
 	public static void main(String[] args) {
 		
-	Scanner kb = new Scanner(System.in);
-	System.out.println("Enter message");
-	String message = kb.next();
-	char c = message.charAt(0);
-	
-	
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter message");
+		String msg = kb.next();
+		int encryption = msg.length();
+		int shift = 3;
+		
+		//cipher()
+	//	System.out.println(cipher("abc", 3);  //prints def
+	//	System.out.println(cipher("xyz", shift);  //prints abc
+		
+	}
+
+	String cipher(String msg, int shift){
+	    String s = "";
+	    int len = msg.length();
+	    for(int x = 0; x < len; x++){
+	        char c = (char)(msg.charAt(x) + shift);
+	        if (c > 'z')
+	            s += (char)(msg.charAt(x) - (26-shift));
+	        else
+	            s += (char)(msg.charAt(x) + shift);
+	    }
+	    return s;
 	}
 }
